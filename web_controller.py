c7b8b23d5c8ddc98194c1f77a67c646c44329745
Oracle Software Owner <oracle@astana31.basu>
2015-08-21 14:36:18 +0600

    create

--- web_controller.py
+++ web_controller.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
+import redis_phones
 
 app = Flask(__name__)
 
 @app.route('/index')
 def index():
     return render_template('index.html')
+	self.con.
     
 @app.route('/set', methods=['POST',])
 def set():
@@ -13,6 +15,7 @@ def set():
         name=request.form.get('name', ''),
         phone=request.form.get('phone', ''),
     )
+
     
     
 if __name__ == '__main__':
