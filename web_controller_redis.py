249e3a14ed6644fc98b8f590d934b09948d02240
Oracle Software Owner <oracle@astana31.basu>
2015-08-21 14:38:43 +0600

    create

---
+++ web_controller_redis.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request,redirect
+import redis_phones
+
+app = Flask(__name__)
+contacts= redis_phones.Contacts(None)
+
+@app.route('/index')
+def index():
+    return render_template('index.html',c=contacts.list_contacts())
+
+@app.route('/set', methods=['POST',])
+def set():
+	try:
+    		contacts.add_contact(request.form.get('name',''),request.form.get('phone',''))
+	except KeyError as e:
+		Warning=str(e)
+   	return redirect(
+        'index'
+    )
+@app.route('/create',methods = ['GET','POST'])
+def create():
+	if request.method == 'GET':
+		return 	render_template('create.html')
+	try:
+		contacts.add_contact(request.form.get('name',''),request.form.get('phone',''))
+	except KeyError as e:
+		return render_template('create.html', warning=str(e))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
